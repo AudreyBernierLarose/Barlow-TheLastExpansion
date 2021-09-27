b1f8ca1d604bc40600efde5b620c85bb03af3dac
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-09-27 19:23:06 -0400

    Started working on Camera and Character Controllers

---
+++ Assets/Scripts/CameraControllerWithBuffer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerWithBuffer : MonoBehaviour
+{
+    //Serialized fields
+    [SerializeField] private Transform player;
+
+    [Range(1.0f, 10.0f)][SerializeField] private float cameraOffsetX = 5.0f;
+    [Range(1.0f, 10.0f)][SerializeField] private float cameraOffsetY = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check the X Threshold 
+        if (player.position.x < transform.position.x - (0.5f * cameraOffsetX)) //Left
+        {
+            transform.position = new Vector3(
+                player.position.x + (0.5f * cameraOffsetX), 
+                transform.position.y, 
+                transform.position.z);
+        }
+        else if (player.position.x > transform.position.x + (0.5f * cameraOffsetX)) //Right
+        {
+            transform.position = new Vector3(
+                player.position.x - (0.5f * cameraOffsetX), 
+                transform.position.y, 
+                transform.position.z);
+        }
+
+        //Check the Y Threshold
+        if (player.position.y < transform.position.y - (0.5f * cameraOffsetY)) // Down
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                player.position.y + (0.5f * cameraOffsetY),
+                transform.position.z);
+        }
+        else if (player.position.y > transform.position.y + (0.5f * cameraOffsetY)) // Up
+        {
+            transform.position = new Vector3(
+                transform.position.x, 
+                player.position.y - (0.5f * cameraOffsetY), 
+                transform.position.z);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(cameraOffsetX, cameraOffsetY, 0.0f));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private fields
+    private Rigidbody2D rBody;
+    private bool isGrounded = false;
+    private Animator anim;
+    //private bool isFacingRight = true;
+
+    //Serialized fields
+    [SerializeField] private float speed;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+
+        //Jump code
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rBody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        //Check if the sprite needs to be flipped
+        if (rBody.velocity.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            //Flip();
+        }
+        else if (rBody.velocity.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            //Flip();
+        }
+
+        //Communicate with the animator
+        anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("yVelocity", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+
+    /*private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
+    }*/
+}
