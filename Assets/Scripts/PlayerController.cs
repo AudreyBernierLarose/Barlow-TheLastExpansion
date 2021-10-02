b303b183de3d184f3423146a867e96a5b43023f5
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-10-02 01:21:17 -0400

    Character movements - running and crouching - and Level 3 middle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,11 @@ public class PlayerController : MonoBehaviour
     //private fields
     private Rigidbody2D rBody;
     private bool isGrounded = false;
+    private bool isCrouching = false;
     private Animator anim;
-    //private bool isFacingRight = true;
 
     //Serialized fields
+    [SerializeField] private float runSpeed;
     [SerializeField] private float speed;
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private float jumpForce;
@@ -46,20 +47,21 @@ public class PlayerController : MonoBehaviour
 
         //Check if the sprite needs to be flipped
         if (rBody.velocity.x > 0)
-        {
             transform.eulerAngles = new Vector3(0, 0, 0);
-            //Flip();
-        }
+        
         else if (rBody.velocity.x < 0)
-        {
             transform.eulerAngles = new Vector3(0, 180, 0);
-            //Flip();
-        }
+
+        
 
         //Communicate with the animator
         anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
         anim.SetFloat("yVelocity", rBody.velocity.y);
         anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isCrouching", isCrouching);
+
+        Run();
+        Crouch();
     }
 
     private bool GroundCheck()
@@ -67,12 +69,29 @@ public class PlayerController : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
     }
 
-    /*private void Flip()
+    void Run()
     {
-        Vector3 temp = transform.localScale;
-        temp.x *= -1;
-        transform.localScale = temp;
+        //RUNNING MOVEMENT
+        if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A)))
+        {
+            speed = runSpeed;
+            anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
+        }
 
-        isFacingRight = !isFacingRight;
-    }*/
+        else
+        {
+            speed = 4.0f;
+            anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));   
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKey(KeyCode.S) && isGrounded)
+        {
+            anim.SetBool("isCrouching", !isCrouching);
+            rBody.velocity = new Vector2(0.0f, 0.0f);
+        }
+        
+    }
 }
