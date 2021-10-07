5f956ff3e1b60bd4c0da357cb74678f0bdaedaa2
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-10-07 16:48:55 -0400

    Implemented Start Collection

---
+++ Assets/Scripts/StarPoints.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarPoints : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Star" && Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("In the Star trigger function");
+            StarScript.starPoints++;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/StarScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarScript : MonoBehaviour
+{
+    public static int starPoints = 0;
+    Text starText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        starText.text = "Stars: " + starPoints;
+    }
+}
