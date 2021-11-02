35ff1e62e3fddf82bca2039b8d49da2bf50c4504
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-11-02 00:41:19 -0400

    Continued Level 3 Mid

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject platform;
+    [SerializeField] private GameObject platform2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            platform.SetActive(true);
+            platform2.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
