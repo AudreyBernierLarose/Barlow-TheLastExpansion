0e47bd96f16003a0791d50e999412e3291bc5413
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2021-11-10 18:39:13 -0500

    Adjusted Camera and Added Menu BG reset

--- Assets/Scripts/BackgroundScrolling.cs
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundScrolling : MonoBehaviour
 {
     public float scrollSpeed;
     private Renderer renderer;
 
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        timer = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        renderer.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0);
+        timer += Time.deltaTime;
+
+        Vector2 offset = new Vector2(timer * scrollSpeed, 0);
+        renderer.material.mainTextureOffset = offset;
     }
 }
